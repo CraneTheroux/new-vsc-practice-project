81754d3a41597e29b38f824407ce3318b70492b0
CraneTheroux <2640082560@qq.com>
2026-05-02 21:40:04 +0800

    initalizing

---
+++ day12.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<algorithm>
+class format{
+public:
+    int operator ()(int val)const{
+        std::cout<<val<<" ";
+    }
+};
+class Solution {
+  std::vector<int> shuffle(std::vector<int> nums, int n) {
+    
+  }
+};
